096beec43b6d18a226cb1fadf38db8205a390bbe
柏凱 趙 <isu10203112a@gmail.com>
2018-02-08 00:31:03 +0800

    add new filter and add myfirst.cpp

---
+++ C-Language_Practices/myfirst.cpp
@@ -0,0 +1,12 @@
+//myfirst.cpp--displays a message
+
+#include <iostream>
+#include <stdafx.h>
+
+int myfirst() {
+	using namespace std;
+	cout << "Come up and C++ me some time.";
+	cout << endl;
+	cout << "You won't regret it!" << endl;
+	return 0;
+}
