e4729a6b867d0d408b049b88764dea484fdd1156
柏凱 趙 <isu10203112a@gmail.com>
2018-02-08 01:17:14 +0800

    add new folder and add myfirst.cpp

---
+++ First step in C++/myfirst.cpp
@@ -0,0 +1,11 @@
+//myfirst.cpp--display a message
+#include <stdafx.h>
+#include <iostream>
+int myfirst() {
+
+	using namespace std;
+	cout << "Come up and C++ me some time.";
+	cout << endl;
+	cout << "You won't regret it!" << endl;
+	return 0;
+}
